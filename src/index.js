df4f4dde022ed5ca4e55c7166102949ee5d454e6
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-11-27 13:20:23 +0100

    Add validation

--- src/index.js
+++ src/index.js
@@ -15,24 +15,29 @@ let temp = 0
 
 
 submitButton.addEventListener('click', (e)=>{
-  loader.classList.remove('hide')
+ 
   e.preventDefault();
   async function weatherInfo(){
     let inputValue = input.value
     searchText.textContent = inputValue.toUpperCase()
-
-    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=f1626be87a0ae483be20b659210c16a3`)
-    response = await response.json()
-    loader.classList.add('hide')
-    mainWeather.textContent = `${response.weather[0].description}`
-    temp = Math.floor((response.main.temp)-273)
-    tempResult.textContent = `${temp}°C`;
-    pressureResult.textContent = `${response["main"]["pressure"]}mm`
-    humidityResult.textContent = `${response["main"]["humidity"]}%`
-    precpitationResult.textContent = `${response.weather[0].main}`
-    windResult.textContent = `${response["wind"]["speed"]}m/s`
-    timeResult.textContent = response["timezone"] < 0 ? `UTC${response["timezone"]/3600}` : `UTC+${response["timezone"]/3600}`
-    return response
+      let regexp = /[a-zA-Z]/
+    if(inputValue.match(regexp)){
+      loader.classList.remove('hide')
+      let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=f1626be87a0ae483be20b659210c16a3`)
+      response = await response.json()
+      loader.classList.add('hide')
+      mainWeather.textContent = `${response.weather[0].description}`
+      temp = Math.floor((response.main.temp)-273)
+      tempResult.textContent = `${temp}°C`;
+      pressureResult.textContent = `${response["main"]["pressure"]}mm`
+      humidityResult.textContent = `${response["main"]["humidity"]}%`
+      precpitationResult.textContent = `${response.weather[0].main}`
+      windResult.textContent = `${response["wind"]["speed"]}m/s`
+      timeResult.textContent = response["timezone"] < 0 ? `UTC${response["timezone"]/3600}` : `UTC+${response["timezone"]/3600}`
+      return response
+    }else{
+      searchText.textContent = "Invalid Input"
+    }
   }
   weatherInfo().catch(error =>{
     console.error(error)
