1bf5eedc43ec95567e0c58f341c0e0003b91e9b1
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-12-01 12:58:00 +0100

    Add more test

--- src/capitalize.test.js
+++ src/capitalize.test.js
@@ -1,6 +1,10 @@
 const capitalize = require("./capitalize");
 
 test('It should capitalize the first letter',()=>{
-  expect(capitalize('bride')).toBe('bride')
+  expect(capitalize('bride')).toBe('Bride')
   expect(capitalize('bride')).not.toBe('bride')
 })
+
+// test('It should be a string', ()=>{
+//   expect(capitalize('bride')).toMatch(capitalize(String))
+// })
